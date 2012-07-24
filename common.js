f9207b5f9452162fe9373308682efa0052348412
Mike Bannister <notimpossiblemike@gmail.com>
2012-07-24 04:39:37 -0400

    do env based auth

--- common.js
+++ common.js
@@ -19,5 +19,17 @@ new Appstrap({
   }
 });
 
-Meteor.accounts.google.config('883542766681-7tgmlmqt2oe8lm7nc56h65n0n4g24am6.apps.googleusercontent.com', 'http://dev.atmospher.es');
-Meteor.accounts.facebook.config('396848297039113', 'http://dev.atmospher.es');
+var url, googleAppId, facebookAppId;
+
+if (Meteor.env.is_development) {
+  url = 'http://dev.atmosphere.meteor.com';
+  googleAppId = '822601694185.apps.googleusercontent.com';
+  facebookAppId = '396848297039113';
+} else if (Meteor.env.is_production) {
+  url = 'https://atmosphere.meteor.com';
+  googleAppId = '822601694185-kscevmtfiip8f5ta9a6eupkmk24d3avm.apps.googleusercontent.com';
+  facebookAppId = '286940654746222';
+}
+
+Meteor.accounts.google.config(googleAppId, url);
+Meteor.accounts.facebook.config(facebookAppId, url);
