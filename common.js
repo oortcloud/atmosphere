d66660d509ded21908e96a25ef21064ce179ba63
Mike Bannister <notimpossiblemike@gmail.com>
2012-07-24 03:28:16 -0400

    prettier

--- common.js
+++ common.js
@@ -1,2 +1,23 @@
+
+new Appstrap({
+  hosts: {
+    production: ['atmosphere.meteor.com']
+  },
+  github: {
+    user: 'possibilities',
+    repo: 'atmosphere',
+    forkMe: {
+      position: 'left'
+    }
+  },
+  tabs: {
+    name: 'navigation',
+    tabs: [
+      'packages',
+      'help'
+    ]
+  }
+});
+
 Meteor.accounts.google.config('883542766681-7tgmlmqt2oe8lm7nc56h65n0n4g24am6.apps.googleusercontent.com', 'http://dev.atmospher.es');
 Meteor.accounts.facebook.config('396848297039113', 'http://dev.atmospher.es');
