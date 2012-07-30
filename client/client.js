ab73e4df77d03699d9486bd959bc2a7902f7e5a4
Mike Bannister <notimpossiblemike@gmail.com>
2012-07-30 18:13:49 -0400

    improved

--- client/client.js
+++ client/client.js
@@ -1,8 +1,39 @@
+(function() {
 
-new ForkMe({
-  user: 'possibilities',
-  repo: 'atmosphere',
-  ribbon: {
-    position: 'left'
-  }
-});
+  new ForkMe({
+    user: 'possibilities',
+    repo: 'atmosphere',
+    ribbon: {
+      position: 'left',
+      color: 'red'
+    }
+  });
+
+  var ifAny = function(data, options) {
+    if (!data || (_.isArray(data) && !data.length) || (_.isFunction(data.fetch) && !data.count()))
+      return options.inverse(this);
+    else
+      return options.fn(this);
+  };
+  
+  Handlebars.registerHelper('ifAny', function(data, options) {
+    return ifAny(data, options);
+  });
+
+  Handlebars.registerHelper('ifEmpty', function(data, options) {
+    return !ifAny(data, options);
+  });
+
+  Handlebars.registerHelper('timeAgo', function(time) {
+    return moment(time).fromNow();
+  });
+
+  Packages = new Meteor.Collection('packages');
+
+  Meteor.subscribe('packages');
+
+  Template.packages.packages = function() {
+    return Packages.find();
+  };
+
+})();
--- server/server.js
+++ server/server.js
@@ -9,3 +9,49 @@ DeployConfig.get('facebookSecret', function(secret) {
 DeployConfig.get('twitterSecret', function(secret) {
   Meteor.accounts.twitter.setSecret(secret);
 });
+
+Packages = new Meteor.Collection('packages');
+Packages.allow({});
+
+Meteor.publish('packages', function() {
+  return Packages.find();
+});
+
+Meteor.methods({
+  publish: function(pkgInfo) {
+    
+    var pkg = Packages.findOne({ name: pkgInfo.name });
+
+    if (pkg) {
+      pkg.latest = pkgInfo.version;
+      pkgInfo.updatedAt = new Date();
+
+      pkg.versions.push({
+        version: pkgInfo.version,
+        packages: pkgInfo.packages
+      });
+
+      delete pkgInfo.packages;
+      delete pkgInfo.version;
+
+      Packages.update(pkg._id, {
+        $set: pkgInfo
+      });
+    } else {
+      pkg = pkgInfo;
+
+      pkg.userId = this.userId();
+      pkgInfo.latest = pkgInfo.version;
+      pkgInfo.createdAt = pkgInfo.updatedAt = new Date();
+      pkgInfo.versions = [{
+        version: pkgInfo.version,
+        packages: pkgInfo.packages
+      }];
+      
+      delete pkgInfo.packages;
+      delete pkgInfo.version;
+
+      Packages.insert(pkgInfo);
+    }
+  }
+});
