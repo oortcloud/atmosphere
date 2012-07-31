7c898abd47e2881158965ca551651e0dc57441a8
Mike Bannister <notimpossiblemike@gmail.com>
2012-07-30 23:19:30 -0400

    removed 3rd party auth

--- server/server.js
+++ server/server.js
@@ -1,15 +1,3 @@
-DeployConfig.get('googleSecret', function(secret) {
-  Meteor.accounts.google.setSecret(secret);
-});
-
-DeployConfig.get('facebookSecret', function(secret) {
-  Meteor.accounts.facebook.setSecret(secret);
-});
-
-DeployConfig.get('twitterSecret', function(secret) {
-  Meteor.accounts.twitter.setSecret(secret);
-});
-
 Packages = new Meteor.Collection('packages');
 Packages.allow({});
 
