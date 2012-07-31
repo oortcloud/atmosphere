773d48f406f7ee7d9e3156746f228407231b55b4
Mike Bannister <notimpossiblemike@gmail.com>
2012-07-31 01:44:06 -0400

    improving

--- server/server.js
+++ server/server.js
@@ -7,9 +7,30 @@ Meteor.publish('packages', function() {
 
 Meteor.methods({
   publish: function(pkgInfo) {
-    
+
+    var allowedFields = [
+      'name',
+      'description',
+      'homepage',
+      'author',
+      'version',
+      'git',
+      'packages'
+    ];
+
+    var requiredFields = [
+      'name',
+      'description',
+      'homepage',
+      'author',
+      'version',
+      'git',
+    ];
+
+    // Let's see if we have a record for the package
     var pkgRecord = Packages.findOne({ name: pkgInfo.name });
 
+    // Ok we have one
     if (pkgRecord) {
       
       if (pkgRecord.userId !== this.userId())
@@ -21,17 +42,19 @@ Meteor.methods({
         // Update
         var lastIndex = pkgRecord.versions.length - 1;
         pkgRecord.versions[lastIndex].version = pkgInfo.version;
-        pkgRecord.versions[lastIndex].packages = pkgInfo.packages;
+        if (pkgInfo.packages)
+          pkgRecord.versions[lastIndex].packages = pkgInfo.packages;
         pkgRecord.versions[lastIndex].updatedAt.push(new Date());
       
       } else {
         // Add new version
         pkgRecord.versions.push({
           version: pkgInfo.version,
-          packages: pkgInfo.packages,
           createdAt: new Date(),
           updatedAt: [new Date()]
         });
+        if (pkgInfo.packages)
+          pkgRecord.packages = pkgInfo.packages;
         pkgRecord.latest = pkgInfo.version;
       }
       
@@ -61,7 +84,6 @@ Meteor.methods({
       delete pkgInfo.packages;
       delete pkgInfo.version;
       
-      console.log(pkgInfo);
       Packages.insert(pkgInfo);
     }
   }
