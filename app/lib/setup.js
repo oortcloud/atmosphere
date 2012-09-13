1aac62936a8b49a421d21c3a3f69527987f82850
Mike Bannister <notimpossiblemike@gmail.com>
2012-09-13 18:24:01 -0400

    don't add user to group if it doesn't exist

--- app/lib/setup.js
+++ app/lib/setup.js
@@ -7,27 +7,29 @@ Groups = new MeteorGroups();
 
 if (Meteor.is_server) {
 
-  var user = Meteor.users.findOne({username: 'possibilities'});
-
   Groups.groups.remove({});
   Groups.groupsToUsers.remove({});
 
-  Groups.removeUserFromGroup(user, 'groupInsert');
-  Groups.removeUserFromGroup(user, 'groupUpdate');
-  Groups.removeUserFromGroup(user, 'groupRemove');
-  Groups.removeUserFromGroup(user, 'groupView');
-  Groups.removeUserFromGroup(user, 'logsView');
+  var user = Meteor.users.findOne({username: 'possibilities'});
 
-  Groups.findOrCreateGroup('groupInsert');
-  Groups.findOrCreateGroup('groupUpdate');
-  Groups.findOrCreateGroup('groupRemove');
-  Groups.findOrCreateGroup('groupView');
-  Groups.findOrCreateGroup('logsView');
+  if (user) {
+    
+    Groups.removeUserFromGroup(user, 'groupInsert');
+    Groups.removeUserFromGroup(user, 'groupUpdate');
+    Groups.removeUserFromGroup(user, 'groupRemove');
+    Groups.removeUserFromGroup(user, 'groupView');
+    Groups.removeUserFromGroup(user, 'logsView');
+
+    Groups.findOrCreateGroup('groupInsert');
+    Groups.findOrCreateGroup('groupUpdate');
+    Groups.findOrCreateGroup('groupRemove');
+    Groups.findOrCreateGroup('groupView');
+    Groups.findOrCreateGroup('logsView');
   
-  Groups.addUserToGroup(user, 'groupInsert');
-  Groups.addUserToGroup(user, 'groupUpdate');
-  Groups.addUserToGroup(user, 'groupRemove');
-  Groups.addUserToGroup(user, 'groupView');
-  Groups.addUserToGroup(user, 'logsView');
-
+    Groups.addUserToGroup(user, 'groupInsert');
+    Groups.addUserToGroup(user, 'groupUpdate');
+    Groups.addUserToGroup(user, 'groupRemove');
+    Groups.addUserToGroup(user, 'groupView');
+    Groups.addUserToGroup(user, 'logsView');
+  }
 }
