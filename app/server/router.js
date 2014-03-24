cf3b10e0e0c13ccef155b0168937f5111be67ea7
Tom Coleman <tom@thesnail.org>
2014-03-24 16:30:08 +1100

    Setup redirects. Ready to go.

---
+++ app/server/router.js
@@ -0,0 +1,20 @@
+// XXX: change this to www.
+var V2_URL = 'http://beta.atmospherejs.com';
+
+var redirectTo = function(path) {
+  return [301, {Location: V2_URL + path}, ''];  
+}
+
+// send routes to V2
+Meteor.Router.add('/', function(name) {
+  return redirectTo('/');
+});
+Meteor.Router.add('/package/:name', function(name) {
+  return redirectTo('/package/' + name);
+});
+Meteor.Router.add('/wtf/app', function(name) {
+  return redirectTo('/docs/installing');
+});
+Meteor.Router.add('/wtf/package', function(name) {
+  return redirectTo('/docs/publishing');
+});
